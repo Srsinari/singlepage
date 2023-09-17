96bf5d015e13869dfc226bdaf290cb65a3db39a3
Srsinari <sunnysinari0018@gmail.com>
2023-09-17 07:48:05 +0530

    ain

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       {/* <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/> */}
       <div className="section">
-    <Router>
+    <Router basename="/BD_wishes">
     <Routes>
       
       
